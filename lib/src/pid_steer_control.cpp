8628e827948804dfd23b3bea3686c0ad251fb98f
josep <boobooboo2@naver.com>
2023-10-11 09:27:50 +0900

    파일 이름 변경

--- lib/src/pid_steer_control.cpp
+++ lib/src/pid_steer_control.cpp
@@ -12,9 +12,9 @@ static const double DEFAULT_WHEEL_BASE = 0.41;                   // 앞 뒤 바�
 static const double DEFAULT_DISTANCE_PID_KP = 0.4;                        // gain
 static const double DEFAULT_DISTANCE_PID_KI = 0.05;
 static const double DEFAULT_DISTANCE_PID_KD = 0.7;
-static const double DEFAULT_STEER_PID_KP = 1.2;
+static const double DEFAULT_STEER_PID_KP = 0.9;
 static const double DEFAULT_STEER_PID_KI = 0.0;
-static const double DEFAULT_STEER_PID_KD = 0.9;
+static const double DEFAULT_STEER_PID_KD = 1.2;
 
 static double distance_between_point_and_line(Point point, Point line_point1, Point line_point2)
 {
@@ -37,22 +37,9 @@ static double distance_between_point_and_line(Point point, Point line_point1, Po
     return error_distance;
 }
 
-int pid_steer_control::test_funtion()
-{
-    std::cout << "testtt" << std::endl;
-    return 0;
-}
-
 pid_steer_control::pid_steer_control()
 {
     this->init(DEFAULT_MAX_STEER, DEFAULT_MAX_SPEED, DEFAULT_WHEEL_BASE);
-    this->path_accel_pid = pid_controller(1, 0, 0);
-    this->path_distance_pid = pid_controller(DEFAULT_DISTANCE_PID_KP, DEFAULT_DISTANCE_PID_KI, DEFAULT_DISTANCE_PID_KD);
-    this->points.clear();
-    this->steer_kp = DEFAULT_STEER_PID_KP;
-    this->steer_ki = DEFAULT_STEER_PID_KI;
-    this->steer_kd = DEFAULT_STEER_PID_KD;
-    this->steer_pre_e = 0;
 }
 
 pid_steer_control::~pid_steer_control()
@@ -60,61 +47,253 @@ pid_steer_control::~pid_steer_control()
 
 }
 
-int pid_steer_control::steering_control(ControlState state, double& steer)
+void pid_steer_control::init(const double max_steer_angle, const double max_speed, const double wheel_base)
 {
-    double e = 0.0f;
-    // std::cout << "update" << std::endl;
-    int current_target_ind = this->calculate_target_index(state, this->points, this->target_ind, e);
-    if (current_target_ind < this->target_ind) {
-        current_target_ind = this->target_ind;
+    this->path_accel_pid = pid_controller(1, 0, 0);
+    this->steer_kp = DEFAULT_STEER_PID_KP;
+    this->steer_ki = DEFAULT_STEER_PID_KI;
+    this->steer_kd = DEFAULT_STEER_PID_KD;
+    this->path_distance_pid = pid_controller(DEFAULT_DISTANCE_PID_KP, DEFAULT_DISTANCE_PID_KI, DEFAULT_DISTANCE_PID_KD);
+    this->points.clear();
+
+    this->max_steer_angle = max_steer_angle;
+    this->max_speed = max_speed;
+    this->wheel_base = wheel_base;
+
+    this->jumping_point_count = 1;
+    this->ref_distance = 0.1; // 10cm
+
+    this->adapted_pid_distance_gain = 1.2;
+    this->adapted_pid_steer_gain = 3;
+}
+
+void pid_steer_control::set_course(ControlState init_state, std::vector<Point> points)
+{
+    int goal_index = points.size() - 1;
+
+    this->init_state = init_state;
+    this->points = points;
+
+    if (this->init_state.yaw - this->points[0].yaw >= M_PI) {
+        this->init_state.yaw -= 2.0f * M_PI;
+    } else if (this->init_state.yaw - this->points[0].yaw <= -M_PI) {
+        this->init_state.yaw += 2.0f * M_PI;
     }
 
-    double yaw_error = pi_2_pi(this->points[current_target_ind].yaw - state.yaw);
+    this->goal_state = ControlState(this->points[goal_index].x, this->points[goal_index].y, this->points[goal_index].yaw, 0, this->points[goal_index].speed);
+    this->t = 0.0f;
+
+    this->target_ind = this->calculate_nearest_index(this->init_state, this->points, 0);
+    this->smooth_yaw(this->points);
+    this->oa.clear();
+    this->odelta.clear();
+}
+
+void pid_steer_control::add_course(ControlState init_state, std::vector<Point> points)
+{
+    this->points.insert(end(this->points), begin(points), end(points));
+
+    int goal_index = points.size() - 1;
+
+    this->init_state = init_state;  // init start state
+    if (this->init_state.yaw - this->points[0].yaw >= M_PI) {
+        this->init_state.yaw -= 2.0f * M_PI;
+    } else if (this->init_state.yaw - this->points[0].yaw <= -M_PI) {
+        this->init_state.yaw += 2.0f * M_PI;
+    }
+    this->goal_state = ControlState(this->points[goal_index].x, this->points[goal_index].y, this->points[goal_index].yaw, 0, this->points[goal_index].speed);
+    this->t = 0.0f;
+
+    this->target_ind = this->calculate_nearest_index(this->init_state, this->points, 0);
+    this->smooth_yaw(this->points);
+    this->oa.clear();
+    this->odelta.clear();
+    this->points.back().speed = 2.2;
+}
+
+int pid_steer_control::calculate_nearest_index(ControlState state, std::vector<Point> points, int pind)
+{
+    const int N_IND_SEARCH = 5;
+    double min = 10000;
+    uint32_t min_point_index = 0;
+    for (uint32_t i = pind; i < (pind + N_IND_SEARCH); i++) {
+        double dx = state.x - points[i].x;
+        double dy = state.y - points[i].y;
+        double point_to_distance = dx * dx + dy * dy;
+        if (min > point_to_distance) {
+            min = point_to_distance;
+            min_point_index = i;
+        }
+        if (i >= (points.size() - 1)) {
+            break;
+        }
+    }
+    return min_point_index;
+}
+
+int pid_steer_control::calculate_target_index(ControlState state, std::vector<Point> points, int pind, double& err_front_axel)
+{
+    const int N_IND_SEARCH = 5;
+    double min = 10000;
+    double target_dx = 0;
+    double target_dy = 0;
+    uint32_t min_point_index = 0;
+
+    double fx = state.x + this->wheel_base * std::cos(state.yaw);
+    double fy = state.y + this->wheel_base * std::sin(state.yaw);
+
+    for (uint32_t i = pind; i < (pind + N_IND_SEARCH); i++) {
+        double dx = fx - points[i].x;
+        double dy = fy - points[i].y;
+        double point_to_distance = dx * dx + dy * dy;
+        if (min > point_to_distance) {
+            min = point_to_distance;
+            target_dx = dx;
+            target_dy - dy;
+            min_point_index = i;
+        }
+        if (i >= (points.size() - 1)) {
+            break;
+        }
+    }
+
+    Point current_state = {this->state.x, this->state.y, 0, 0, 0};
+    if (min_point_index == 0) {
+        err_front_axel = 0;
+    } else {
+        err_front_axel = distance_between_point_and_line(current_state, this->points[min_point_index - 1], this->points[min_point_index]);
+    }
+
+    return min_point_index;
+}
+
+void pid_steer_control::smooth_yaw(std::vector<Point> &points)
+{
+    for (uint32_t i = 0; i < points.size() - 1; i++) {
+        double diff_yaw = points[i + 1].yaw - points[i].yaw;
+
+        while (diff_yaw >= M_PI_2) {
+            points[i + 1].yaw -= M_PI * 2.0;
+            diff_yaw = points[i + 1].yaw - points[i].yaw;
+        }
+
+        while (diff_yaw <= -M_PI_2) {
+            points[i + 1].yaw += M_PI * 2.0;
+            diff_yaw = points[i + 1].yaw - points[i].yaw;
+        }
+    }
+}
+
+int pid_steer_control::pid_steering_control(ControlState state, int target_index, double distance_error, double& steer)
+{
+    double yaw_error = 0;
+
+    if (abs(distance_error) < this->ref_distance) {
+        path_distance_pid.set_target(distance_error);
+        yaw_error = pi_2_pi((this->points[target_index].yaw - state.yaw)) * this->adapted_pid_distance_gain;
+    } else {
+        path_distance_pid.set_target(distance_error * this->adapted_pid_steer_gain);
+        yaw_error = pi_2_pi(this->points[target_index].yaw - state.yaw);
+    }
+
     double th_e = pi_2_pi(yaw_error * this->steer_kp + (yaw_error - this->steer_pre_e) * this->steer_kd);
 
-    this->path_distance_pid.set_target(e);
     double steer_delta = std::atan2(this->path_distance_pid.calculate(0), state.v);
 
     steer = th_e + steer_delta;
 
     steer_pre_e = yaw_error;
 
-    return current_target_ind;
+    return target_index;
 }
 
-int pid_steer_control::velocity_control(ControlState state, double& accel)
-{
-    this->path_accel_pid.set_target(this->points[this->target_ind].speed);
-    accel = this->path_accel_pid.calculate(this->state.v);
+bool pid_steer_control::update(double dt) {
+    double calculated_steer = 0;
+    double distance_error;
+    this->dt = dt;
 
-    return 0;
-}
-
-void pid_steer_control::set_gain(int gain_index, double* gain_value)
-{
-    if (gain_index == PATH_TRACKER_PID_TYPE_DISTANCE) {
-        this->path_distance_pid.set_gain(gain_value[0], gain_value[1], gain_value[2]);
-    } else if (gain_index == PATH_TRACKER_PID_TYPE_STEER) {
-        this->steer_kp = gain_value[0];
-        // this->steer_ki = gain_value[1];
-        this->steer_kd = gain_value[2];
-    } else {
-
+    if (dt == 0) {
+        return false;
     }
-}
 
-void pid_steer_control::get_gain(int gain_index, double* gain_value)
-{
-    if (gain_index == PATH_TRACKER_PID_TYPE_DISTANCE) {
-        gain_value[0] = this->path_distance_pid.get_p_gain();
-        gain_value[1] = this->path_distance_pid.get_i_gain();
-        gain_value[2] = this->path_distance_pid.get_d_gain();
-    } else if (gain_index == PATH_TRACKER_PID_TYPE_STEER) {
-        gain_value[0] = this->steer_kp;
-        gain_value[1] = this->steer_ki;
-        gain_value[2] = this->steer_kd;
-    } else {
-
+    this->target_ind = this->calculate_target_index(state, this->points, this->target_ind, distance_error);
+    int jumped_point = target_ind + this->jumping_point_count;
+    if (target_ind > (this->points.size() - 1) - this->jumping_point_count) {
+        jumped_point = this->points.size() - 1;
     }
+
+
+    pid_steering_control(this->state, jumped_point, distance_error, calculated_steer);
+    // PID로 가속도 값 계산
+    this->path_accel_pid.set_target(this->points[jumped_point].speed);
+    double calculated_accel = this->path_accel_pid.calculate(this->state.v);
+
+    // state update
+    this->state = this->update_state(this->state, calculated_accel, calculated_steer, this->dt);
+    this->state.v = this->points[jumped_point].speed;
+    double state_to_goal_distance = sqrt(pow(this->goal_state.x - this->state.x, 2) + pow(this->goal_state.y - this->state.y, 2));
+    size_t remain_point = get_remain_point();
+    if (remain_point == 0) {
+        // finish
+        return true;
+    }
+
+    return false;
 }
 
+double pid_steer_control::calculate_error()
+{
+    double distance1 = 100;
+    double distance2 = 100;
+    int temp_target_index = this->target_ind - 10;
+
+    if (temp_target_index < 0) {
+        temp_target_index = 0;
+    }
+
+    int nearest_index = this->calculate_nearest_index(state, this->points, temp_target_index);
+
+    auto cal_diatance_between_line_to_point = [](double x1, double y1, double x2, double y2, double stand_x, double stand_y ) -> double {
+        double a = (y1 - y2) / (x1 - x2);
+        double b = -1;
+        double c = y1 - a * x1;
+
+        return abs(a * stand_x + b * stand_y + c) / sqrt(a*a + 1);
+    };
+
+    if (nearest_index != 0) {
+        distance1 = cal_diatance_between_line_to_point(this->points[nearest_index].x, this->points[nearest_index].y,
+                                           this->points[nearest_index - 1].x, this->points[nearest_index - 1].y,
+                                           this->state.x, this->state.y);
+    }
+    if (nearest_index != (this->points.size() - 1)) {
+        distance2 = cal_diatance_between_line_to_point(this->points[nearest_index].x, this->points[nearest_index].y,
+                                           this->points[nearest_index + 1].x, this->points[nearest_index + 1].y,
+                                           this->state.x, this->state.y);
+    }
+    return distance1 < distance2 ? distance1 : distance2;
+}
+
+ControlState pid_steer_control::update_state(ControlState state, double accel, double steer_delta, double dt)
+{
+    if (steer_delta > this->max_steer_angle) {
+        steer_delta = this->max_steer_angle;
+    } else if (steer_delta < -this->max_steer_angle) {
+        steer_delta = -this->max_steer_angle;
+    }
+    state.steer = steer_delta;
+    // golfcar position, angle update
+    state.x = state.x + state.v * std::cos(state.yaw) * dt;
+    state.y = state.y + state.v * std::sin(state.yaw) * dt;
+    state.yaw = state.yaw + state.v / this->wheel_base * std::tan(steer_delta) * dt;
+    // state.v = state.v + accel * dt;
+    // state.v = this->points[this->target_ind].speed;
+
+    if (state.v > this->max_speed) {
+        state.v = this->max_speed;
+    } else if (state.v < -this->max_speed) {
+        state.v = -this->max_speed;
+    }
+
+    return state;
+}
