a6643495509f5325c77ba6e07f4cf16b953a6eee
Jihun Kim <lego8421@gmail.com>
2024-05-22 11:00:32 +0900

    해더 분리

--- lib/inc/path_manager.h
+++ lib/inc/path_manager.h
@@ -5,54 +5,7 @@
 #include <stdint.h>
 #include <vector>
 
-
-#define PT_M_PI    3.14159265358979323846
-#define PT_M_PI_2  1.57079632679489661923
-
-/**
- * @brief 경로 트레킹을 위한 업데이트 함수 결과
- * @details 주행 중, 목표 도착 등 업데이트 함수 결과 값
- */
-typedef enum path_tracker_update_result
-{
-    PT_UPDATE_RESULT_RUNNING = 0x0000,
-    PT_UPDATE_RESULT_GOAL,
-    PT_UPDATE_RESULT_NOT_FOUND_TARGET,
-    PT_UPDATE_RESULT_INVAILED_TIME,
-    PT_UPDATE_RESULT_NOT_READY,
-} pt_update_result_t;
-
-/**
- * @brief 경로 위치 정보
- * @details 경로의 각 점들에 대한 목표 및 상태 값들을 저장
- */
-typedef struct path_point
-{
-    double x;
-    double y;
-    float yaw;
-    float k;
-    float speed;
-} path_point_t;
-
-/**
- * @brief 현재 차량의 상태
- * @details 현재 차량의 상태를 저장
- */
-typedef struct path_tracker_control_state
-{
-    double x;
-    double y;
-    float yaw;
-    float steer;
-    float v;
-} pt_control_state_t;
-
-#define PT_GAIN_TYPE_PID_DISTANCE   0
-#define PT_GAIN_TYPE_PID_STEER      1
-#define PT_GAIN_TYPE_LQR_Q          0
-#define PT_GAIN_TYPE_LQR_R          1
-typedef int pt_gain_type_t;
+#include "path_manager_def.h"
 
 class path_tracker
 {
@@ -349,5 +302,4 @@ public:
      * @TODO 직선일 경우 구분해야함
      */
     static path_point_t get_path_circle(path_point_t point1, path_point_t point2, float slope);
-
 };
