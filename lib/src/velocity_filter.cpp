8c91e46debf911923b0b8270669dc92e04433598
josep <boobooboo2@naver.com>
2024-10-16 18:11:39 +0900

    velocity filter 레거시 제거

--- lib/src/velocity_filter.cpp
+++ lib/src/velocity_filter.cpp
@@ -19,15 +19,6 @@ typedef struct velocity_filter_context_
     float S_inv;      /** 칼만 게인 계산용 */
     float z;
 
-    std::vector<float> v_estimate_buf;
-    uint32_t v_estimate_buf_max_size;
-
-    std::vector<float> x_residual_buf;
-    uint32_t x_residual_buf_max_size;
-
-    std::vector<float> yaw_v_estimate_buf;
-    uint32_t yaw_v_estimate_buf_max_size;
-
     float last_update_time;
     float chi_square_value;
     int init_flag;
@@ -35,60 +26,10 @@ typedef struct velocity_filter_context_
 
 velocity_filter_context_t velocity_estimate_filter;
 
-
-static float H_array_quality0 = 1; /** */
-
-static float H_array_quality1 = 1;
-
-static float H_array_quality2 = 1;
-
-static float H_array_quality3 = 1;
-
-
-// Z 모두
-// 현재 대각 값 외에는 설정하지 않음
-static float R_array_quality0 = 1;
-
-// float R_array_quality0[25] = {0.0204, 0.0, 0.0012, 0.0, 0.0,
-//                               0.0   , 0.0, 0.    , 0.0, 0.0,
-//                               0.0012, 0.0, 0.0094, 0.0, 0.0,
-//                               0.0   , 0.0, 0.0   , 0.0, 0.0,
-//                               0.0   , 0.0, 0.0   , 0.0, 0.0};
-
-// Z중 YAW 제외
-static float R_array_quality1 = 1;
-
-// Z중 V, YAW 제외
-static float R_array_quality2 = 1;
-
-static float R_array_quality3 = 1;
-
-// float R_array_quality3[1] = {0.01};
-
-
 /*
 kalman filter innovation -> 측정 값과 실제 값 차이
 kalman filter residual ->   예측 값과 측정 값 차이
 */
-
-/*
-GPS Quality indicator:
-0: Fix not valid
-1: GPS fix
-2: Differential GPS fix (DGNSS), SBAS, OmniSTAR VBS, Beacon, RTX in GVBS mode
-3: Not applicable
-4: RTK Fixed, xFill
-5: RTK Float, OmniSTAR XP/HP, Location RTK, RTX
-6: INS Dead reckoning
-*/
-
-/**
- * @brief
- * @param x0
- * @param input
- * @return ModelMatrix
- */
-static ModelMatrix state_equation_jacobi(ModelMatrix x0, ModelMatrix input);
 
 /**
  * @brief
@@ -100,8 +41,6 @@ static float estimate(float z);
 
 bool velocity_filter_init()
 {
-    // @todo
-
     velocity_estimate_filter.P = 0;
     velocity_estimate_filter.predict_P = 0;
     velocity_estimate_filter.estimate_P = 0;
@@ -163,7 +102,6 @@ float velocity_filter_predict_state(float accel_x, float updated_time)
 
 bool velocity_filter_estimate_state(float gps_velocity)
 {
-    // z format = [gps v, yaw rate, gps slip+yaw, gps x, gps y]
     float sigma = 10; // 값의 유효성 기준, 크면 클 수록 통과하기 좋음
     float innovation = 0;
     bool result = false;
@@ -194,7 +132,6 @@ float estimate(float z)
                                             velocity_estimate_filter.K * (z - velocity_estimate_filter.predict_x);
     velocity_estimate_filter.estimate_P = velocity_estimate_filter.predict_P - velocity_estimate_filter.K * velocity_estimate_filter.predict_P;
 
-    // velocity_estimate_filter.R = C_hat / velocity_estimate_filter.v_estimate_buf.size() + velocity_estimate_filter.estimate_P;
     velocity_estimate_filter.x = velocity_estimate_filter.estimate_x;
     velocity_estimate_filter.predict_x = velocity_estimate_filter.estimate_x;
     velocity_estimate_filter.P = velocity_estimate_filter.estimate_P;
